6bdb7682274c865d6b22fd2ea744e2ca0e29333b
Andrea Censi <censi@mit.edu>
2015-10-12 15:53:59 -0400

    befor fix

--- src/mocdp/comp/tests/test_connect2.py
+++ src/mocdp/comp/tests/test_connect2.py
@@ -1,37 +1,80 @@
 # -*- coding: utf-8 -*-
 
-#                                                                    
-# > | Exception: connect2() failed                                                                                           
-# > |      ndp2: Instance of <class 'mocdp.comp.wrap.SimpleWrap'>.                                                           
-# > |            Wrap(['a', 'b']|Multiply(R[s]×R[W]→R[J])|['res'])                                                        
-# > | connections: Instance of <type 'set'>.                                                                                 
-# > |              set([Connection(dp1='group2', s1='actuation_power', dp2='mult1', s2='b')])                                
-# > |      ndp1: Instance of <class 'mocdp.comp.wrap.SimpleWrap'>.                                                           
-# > |            Wrap(['extra_payload', 'p0']|Series(Series(Series(Series(Parallel(Id(R[g]), Id(R[g])), Mux(R[g]×R[g] → R[g]×R[g], [(1,), (0,)])), Sum(R[g])), Mux(R[g] → R[g], ())), Mobility(R[g]→R[W]))|['actuation_power'])
-# > |     split: Instance of <type 'set'>.                                                                                   
-# > |            set([])                                                               
-
 from comptests.registrar import comptest
-from mocdp.posets.rcomp import Rcomp
-from mocdp.dp.primitive import PrimitiveDP
-from mocdp.comp.wrap import SimpleWrap
 from mocdp.comp.connection import Connection, connect2
+from mocdp.comp.wrap import SimpleWrap
+from mocdp.dp.primitive import PrimitiveDP
+from mocdp.posets import PosetProduct, Rcomp
+
 
 def get_dummy(fnames, rnames):
     
-    base = (Rcomp,)
-    F = base * len(fnames)
-    R = base * len(rnames)
+    base = (Rcomp(),)
+    F = PosetProduct(base * len(fnames))
+    R = PosetProduct(base * len(rnames))
     
     class Dummy(PrimitiveDP):
         def __init__(self):
             PrimitiveDP.__init__(self, F=F, R=R)
         def solve(self, _func):
             return self.R.bottom()
+    if len(fnames) == 1:
+        fnames = fnames[0]
+    if len(rnames) == 1:
+        rnames = rnames[0]
     return SimpleWrap(Dummy(), fnames, rnames)
     
 @comptest
-def check_connect2():
+def check_connect2_a():
+    #
+    #  f2 -> |  | -r2:F0-> | c2 | -> R0
+
+    ndp1 = get_dummy(['f2'], ['r2'])
+    ndp2 = get_dummy(['F0'], ['R0'])
+
+    connections = [Connection('-', 'r2', '-', 'F0')]
+
+    res = connect2(ndp1, ndp2, connections, split=set())
+
+    assert res.get_fnames() == ['f2']
+    assert res.get_rnames() == ['R0']
+
+
+@comptest
+def check_connect2_b():
+    #  f1 -> | 1| --r1---------------->
+    #  f2 -> | 1 | -r2:F0-> | c2 | -> R0
+
+    ndp1 = get_dummy(['f1', 'f2'], ['r1', 'r2'])
+    ndp2 = get_dummy(['F0'], ['R0'])
+
+    connections = [Connection('-', 'r2', '-', 'F0')]
+
+    res = connect2(ndp1, ndp2, connections, split=set())
+
+    assert res.get_fnames() == ['f1', 'f2']
+    assert res.get_rnames() == ['r1', 'R0']
+
+
+
+@comptest
+def check_connect2_c():
+    #  f1 -> | 1| --r1---------------->
+    #  f2 -> | 1 | -r2:F0-> | c2 | -> R0
+    #        | 1| --r3---------------->
+
+    ndp1 = get_dummy(['f1', 'f2'], ['r1', 'r2'])
+    ndp2 = get_dummy(['F0'], ['R0'])
+
+    connections = [Connection('-', 'r2', '-', 'F0')]
+
+    res = connect2(ndp1, ndp2, connections, split=set())
+
+    assert res.get_fnames() == ['f1', 'f2']
+    assert res.get_rnames() == ['r1', 'r3', 'R0']
+
+@comptest
+def check_connect2_d():
     # 
     #  f0 -> |  | -----> r0
     #  f1 -> |c1| -----> r1
@@ -40,8 +83,8 @@ def check_connect2():
     
     
     ndp1 = get_dummy(['f0', 'f1', 'f2'], ['r0, r1, r2'])
-    ndp1 = get_dummy(['F0', 'F1'], ['R0', 'R1'])
+    ndp2 = get_dummy(['F0', 'F1'], ['R0', 'R1'])
     
-    connections = Connection('-', 'r2', '-', 'F0')
+    connections = [Connection('-', 'r2', '-', 'F0')]
 
-    res = connect2(ndp1, ndp1, connections, split=set())
+    res = connect2(ndp1, ndp2, connections, split=set())
