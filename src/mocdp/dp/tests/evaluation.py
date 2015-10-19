fbae2be5079cdc777e876ae918cfc8149fe4793c
Andrea Censi <censi@mit.edu>
2015-10-18 22:42:36 -0400

    done

--- src/mocdp/dp/tests/evaluation.py
+++ src/mocdp/dp/tests/evaluation.py
@@ -93,58 +93,123 @@ def check_evaluation():
     assert_equal(dp.get_res_space(), SpaceProduct(()))
     assert_equal(dp.get_fun_space(), SpaceProduct(()))
 
-#     # this is wrong
-#     dp.check_feasible((), (0.9, 0.0), ())
+    assert_feasible(dp, (), (0.0, 0.0), ())
+    assert_feasible(dp, (), (1.0, 1.0), ())
+    assert_unfeasible(dp, (), (0.0, 1.0), ())
+    assert_unfeasible(dp, (), (0.0, 0.9), ())
+    assert_feasible(dp, (), (0.5, 0.5), ())
 
-    def assert_check_feasible_raises(*args):
-        try:
-            dp.check_feasible(*args)
-        except NotFeasible:
-            pass
-        else:
-            msg = 'Expected check_feasible() to raise NotFeasible.'
-            msg += 'args = %s' % str(args)
-            raise Exception(msg)
+    assert_unfeasible(dp, (), (1.0, 0.0), ())
+    assert_unfeasible(dp, (), (1.1, 1.1), ())
 
-    def assert_check_unfeasible_raises(*args):
-        try:
-            dp.check_unfeasible(*args)
-        except Feasible:
-            pass
-        else:
-            msg = 'Expected check_unfeasible() to raise Feasible.'
-            msg += 'args = %s' % str(args)
-            raise Exception(msg)
-
-#     dp.check_unfeasible((), (0.0, 0.0), ())
-
-    def assert_feasible(*args):
-        dp.check_feasible(*args)
-        assert_check_unfeasible_raises(*args)
-    def assert_unfeasible(*args):
-        dp.check_unfeasible(*args)
-        assert_check_feasible_raises(*args)
-
-
-    assert_feasible((), (0.0, 0.0), ())
-    assert_feasible((), (1.0, 1.0), ())
-    assert_unfeasible((), (0.0, 1.0), ())
-    assert_unfeasible((), (0.0, 0.9), ())
-    assert_feasible((), (0.5, 0.5), ())
-
-
-    assert_unfeasible((), (1.0, 0.0), ())
-    assert_unfeasible((), (1.1, 1.1), ())
-
-    assert_unfeasible((), (0.9, 0.0), ())
+    assert_unfeasible(dp, (), (0.9, 0.0), ())
 
 
     import numpy as np
     xs = np.linspace(0, 1.5, 30)
     ys = np.linspace(0, 1.5, 30)
-    
-    for y in reversed(ys):
+    print_diagram(dp, xs, ys)
 
+
+#
+# y =      1.034  000000000000000000000000000000
+# y =      0.982  000000000000000000010000000000
+# y =      0.931  000000000000000001100000000000
+# y =      0.879  000000000000000111100000000000
+# y =      0.827  000000000000001111000000000000
+# y =      0.775  000000000000111111000000000000
+# y =      0.724  000000000001111110000000000000
+# y =      0.672  000000000111111100000000000000
+# y =      0.620  000000001111111100000000000000
+# y =      0.568  000000011111111000000000000000
+# y =      0.517  000000111111110000000000000000
+# y =      0.465  000001111111110000000000000000
+# y =      0.413  000011111111100000000000000000
+# y =      0.362  000111111111000000000000000000
+# y =      0.310  001111111110000000000000000000
+# y =      0.258  001111111100000000000000000000
+# y =      0.206  011111111000000000000000000000
+# y =      0.155  011111110000000000000000000000
+# y =      0.103  011111100000000000000000000000
+# y =      0.051  011110000000000000000000000000
+# y =        0.0  100000000000000000000000000000
+
+
+@comptest
+def check_evaluation2():
+    ndp = parse_ndp("""
+    cdp {  
+    f =  cdp {
+        provides a [R]
+            
+        requires c [R]
+            
+        c >= sqrt(a)
+    }
+
+    f.a >= sqrt(f.c)
+  
+  }
+    """)
+    dp = ndp.get_dp()
+    print(dp.repr_long())
+    M = dp.get_imp_space_mod_res()
+
+    assert_equal(M, SpaceProduct((R_dimensionless, R_dimensionless)))
+    assert_equal(dp.get_res_space(), SpaceProduct(()))
+    assert_equal(dp.get_fun_space(), SpaceProduct(()))
+
+    assert_feasible(dp, (), (0.0, 0.0), ())
+    assert_feasible(dp, (), (1.0, 1.0), ())
+    assert_feasible(dp, (), (1.1, 1.1), ())
+#     assert_unfeasible(dp, (), (0.0, 1.0), ())
+#     assert_unfeasible(dp, (), (0.0, 0.9), ())
+    assert_unfeasible(dp, (), (0.5, 0.5), ())
+    assert_unfeasible(dp, (), (2.0, 1.0), ())
+    assert_unfeasible(dp, (), (1.0, 2.0), ())
+#
+#     assert_unfeasible(dp, (), (1.0, 0.0), ())
+#     assert_unfeasible(dp, (), (1.1, 1.1), ())
+#
+#     assert_unfeasible(dp, (), (0.9, 0.0), ())
+
+
+    import numpy as np
+    xs = np.linspace(0, 3.5, 30)
+    ys = np.linspace(0, 3.5, 30)
+    print_diagram(dp, xs, ys)
+
+def assert_check_feasible_raises(dp, *args):
+    try:
+        dp.check_feasible(*args)
+    except NotFeasible:
+        pass
+    else:
+        msg = 'Expected check_feasible() to raise NotFeasible.'
+        msg += 'args = %s' % str(args)
+        raise Exception(msg)
+
+def assert_check_unfeasible_raises(dp, *args):
+    try:
+        dp.check_unfeasible(*args)
+    except Feasible:
+        pass
+    else:
+        msg = 'Expected check_unfeasible() to raise Feasible.'
+        msg += 'args = %s' % str(args)
+        raise Exception(msg)
+
+def assert_feasible(dp, *args):
+    dp.check_feasible(*args)
+    assert_check_unfeasible_raises(dp, *args)
+
+def assert_unfeasible(dp, *args):
+    dp.check_unfeasible(*args)
+    assert_check_feasible_raises(dp, *args)
+
+def print_diagram(dp, xs, ys):
+    s = ""
+    for y in reversed(ys):
         res = []
         for x in xs:
             f = ()
@@ -170,5 +235,7 @@ def check_evaluation():
 
         res = "".join(str(int(w))   for w in res)
         line = 'y = %10.5s  ' % y + "".join(res)
+        s += '\n' + line
         print(line)
+    return s
 
