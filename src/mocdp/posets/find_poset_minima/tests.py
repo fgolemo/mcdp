790a7f40f2618972ad94f99bab3363fa940f5618
Andrea Censi <censi@mit.edu>
2015-11-27 08:20:50 -0500

    comptest

--- src/mocdp/posets/find_poset_minima/tests.py
+++ src/mocdp/posets/find_poset_minima/tests.py
@@ -1,11 +1,36 @@
 # -*- coding: utf-8 -*-
-
 from comptests.registrar import comptest
+import random
+from contracts import contract
+from mocdp.posets.nat import Nat
+from mocdp.posets.poset_product import PosetProduct
+from mocdp.posets.find_poset_minima.baseline_n2 import poset_minima
+
+
+@contract(n='>=1', ndim='>=1', m='>0')
+def get_random_points(n, ndim, m):
+    """ Gets a set of n points in [0,m]*[0,m]*... (ndim dimensions) """
+    res = set()
+    for _ in range(n):
+        s = []
+        for _ in range(ndim):
+            x = random.randint(0, m)
+            s.append(x)
+        r = tuple(s)
+        res.add(r)
+    return res
 
 
 @comptest
 def pmin1():
-    pass
+    n = 100
+    ndim = 2
+    m = 43
+    Ps = get_random_points(n, ndim, m)
+    N2 = PosetProduct(Nat(), Nat())
+
+    r = poset_minima(Ps, N2.leq)
+    print r
 
 
 @comptest
