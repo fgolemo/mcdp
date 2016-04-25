0bcaa144f5b11410b928687f8616c67529d3b078
Andrea Censi <censi@mit.edu>
2016-04-25 17:09:15 -0400

    tests

--- src/mocdp/posets/find_poset_minima/tests.py
+++ src/mocdp/posets/find_poset_minima/tests.py
@@ -6,9 +6,9 @@ from mocdp.posets.find_poset_minima.baseline_n2 import (poset_minima,
 from mocdp.posets.nat import Nat
 from mocdp.posets.poset import Poset
 from mocdp.posets.poset_product import PosetProduct
-import random
 from mocdp.posets.rcomp import Rcomp
 import numpy as np
+import random
 
 @contract(n='>=1', ndim='>=1', m='>0')
 def get_random_points(n, ndim, m):
@@ -24,19 +24,28 @@ def get_random_points(n, ndim, m):
         res.add(r)
     return res
 
-
-@contract(P=Poset, xs='set')
-def stats_for_poset_minima(P, xs, f, maxleq=10000):
-    """
-        
-        f(P, x)
-    """
-    class P2():
+def wrap_with_counts(P, maxleq=None):
+    if isinstance(P, PosetProduct):
+        base = PosetProduct
+    else:
+        base = Poset
+    class P2(base):
         def __init__(self, P):
             self.P = P
+            self.reset_counters()
+
+        def reset_counters(self):
             self.nleq = 0
             self.nleq_true = 0
             self.nleq_false = 0
+
+        def belongs(self, x):
+            return self.P.belongs(x)
+        def check_equal(self, a, b):
+            return self.P.check_equal(a, b)
+
+        def check_leq(self, a, b):
+            return self.check_leq(a, b)
 
         def leq(self, a, b):
             res = self.P.leq(a, b)
@@ -45,26 +54,37 @@ def stats_for_poset_minima(P, xs, f, maxleq=10000):
             else:
                 self.nleq_false += 1
             self.nleq += 1
-            if self.nleq >= maxleq:
-                msg = ('%d leqs (true: %d, false: %d)' % (self.nleq, self.nleq_true,
-                                                                   self.nleq_false))
-                raise ValueError(msg)
+            if maxleq is not None:
+                if self.nleq >= maxleq:
+                    msg = ('%d leqs (true: %d, false: %d)' %
+                           (self.nleq, self.nleq_true, self.nleq_false))
+                    raise ValueError(msg)
             return res
 
         def __getattr__(self, method_name):
             return getattr(P, method_name)
+    return P2(P)
 
-    P_ = P2(P)
+@contract(P=PosetProduct, xs='set')
+def stats_for_poset_minima(P, xs, f, maxleq):
+    """
+        
+        f(P, x)
+    """
+    for s in P:
+        s.reset_counters()
     xs_ = list(xs)
     random.shuffle(xs_)
-    res = f(P_, xs_)
+    res = f(P, xs_)
 
     print('n: %d' % len(xs))
     print('nres: %d' % len(res))
-    print('nleq:  %d' % P_.nleq)
-    print('nleqt: %d' % P_.nleq_true)
-    print('nleqf: %d' % P_.nleq_false)
 
+    for s in P:
+        print('component %s' % s)
+        print(' nleq:  %d' % s.nleq)
+        print(' nleqt: %d' % s.nleq_true)
+        print(' nleqf: %d' % s.nleq_false)
     return res
 
 
@@ -105,23 +125,77 @@ def get_random_antichain(n, ndim):
 
 @comptest
 def pmin2():
-    n = 1000
+    n = 300
     ndim = 2
-    Pbase = Rcomp()
+    Pbase = wrap_with_counts(Rcomp())
     N2 = PosetProduct((Pbase,) * ndim)
 
     print('Using random antichain')
     # point_generation = lambda n: get_random_points(n, ndim, m)
     # Ps = get_random_antichain(n, point_generation, N2.leq)
-    Ps = get_random_antichain(n, ndim)
-    method = poset_minima_n2
-    r = stats_for_poset_minima(N2, Ps, method)
-    print r
+    P1 = get_random_antichain(n, ndim)
+    P2 = get_random_antichain(n, ndim)
+    Ps = set()
+    Ps.update(P1)
+    Ps.update(P2)
 
+    run_all(N2, Ps)
+
+def run_all(poset, Ps):
+    method = poset_minima_n2
+    print('method: %s' % method)
+    r = stats_for_poset_minima(poset, Ps, method, maxleq=None)
+    def poset_minima_n2_sort_first(P, ps):
+        f = sorted(ps)
+        return poset_minima_n2(P, f)
+
+    method = poset_minima_n2_sort_first
+    print('method: %s' % method)
+    r = stats_for_poset_minima(poset, Ps, method, maxleq=None)
+
+    method = poset_minima_n2_optimizedPP
+    print('method: %s' % method)
+    r = stats_for_poset_minima(poset, Ps, method, maxleq=None)
 
 @comptest
 def pmin3():
-    pass
+    n = 200000
+    ndim = 2
+    Pbase = wrap_with_counts(Rcomp())
+    N2 = PosetProduct((Pbase,) * ndim)
+
+    print('Using random antichain')
+    Ps = get_random_points(n, ndim, m=1000)
+
+    run_all(N2, Ps)
+
+
+def poset_minima_n2_optimizedPP(P, ps):
+    assert isinstance(P, PosetProduct) and len(P) == 2
+    l1 = P[0].leq
+    l2 = P[1].leq
+    # Sort by x
+    def cmp1(a, b):
+        if l1(a, b):
+            return -1
+        if l1(b, a):
+            return +1
+        return 0
+
+    sx = sorted(ps, cmp=cmp1)
+    res = set()
+    min_y = None
+    for p in sx:
+        # print('p: %s min_y: %s' % (str(p), min_y))
+        if min_y is None or (l2(p[1], min_y)):
+            res.add(p)
+            min_y = p[1]
+        
+    return res
+    
+
+
+
 
 
 @comptest
