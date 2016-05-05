c27ba85d94a34ec782f145dc237e7ee876a90703
Andrea Censi <censi@mit.edu>
2016-05-04 21:41:01 -0400

    bump

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(name='PyMCDP',
         'PyContracts>=1.7.6',
         'ConfTools>=1.7',
         'QuickApp>=1.2',
-        'RepRep>=2.9.2',
+        'RepRep>=2.9.3',
         # 'gvgen==0.9.1',
       ],
       dependency_links  = [
