dd27809ddea6e2a4ea99ba710f939c37a2609591
Andrea Censi <censi@mit.edu>
2016-06-04 12:09:06 +0200

    done

--- src/mcdp_library/library.py
+++ src/mcdp_library/library.py
@@ -55,24 +55,27 @@ class MCDPLibrary():
         realpath = f['realpath']
 
         def actual_load():
-
             logger.debug('Parsing %r' % id_ndp)
-
-            def load(load_arg):
-                _c, res = self.load_ndp(load_arg)
-                return res
-
-            context = Context()
-            context.load_ndp_hooks = [load]
-            try:
-                result = parse_ndp(data, context=context)
-            except (DPSyntaxError, DPSemanticError) as e:
-                raise e.with_filename(realpath)
-
-            return result
+            return self._actual_load(data, realpath)
 
         cache_file = os.path.join('_cached', '%s.cached' % id_ndp)
         return memo_disk_cache2(cache_file, data, actual_load)
+
+    def _actual_load(self, data, realpath=None):
+        def load(load_arg):
+            _c, res = self.load_ndp(load_arg)
+            return res
+
+        context = Context()
+        context.load_ndp_hooks = [load]
+        try:
+            result = parse_ndp(data, context=context)
+        except (DPSyntaxError, DPSemanticError) as e:
+            if realpath is not None:
+                raise e.with_filename(realpath)
+            else:
+                raise e
+        return result
 
     @contract(returns='set(str)')
     def get_models(self):
@@ -113,10 +116,22 @@ class MCDPLibrary():
         """ Adds the directory to the search directory list. """
         files_mcdp = locate_files(directory=d, pattern='*.mcdp', followlinks=True)
         for f in files_mcdp:
-            basename = os.path.basename(f)
-            data = open(f).read()
-            realpath = os.path.realpath(f)
-            res = dict(data=data, realpath=realpath)
-            self.file_to_contents[basename] = res
+            self._update_file(f)
+            
+    def _update_file(self, f):
+        basename = os.path.basename(f)
+        data = open(f).read()
+        realpath = os.path.realpath(f)
+        res = dict(data=data, realpath=realpath)
+        self.file_to_contents[basename] = res
 
-
+    def write_to_model(self, model_name, data):
+        basename = model_name + '.mcdp'
+        d = self._get_file_data(basename)
+        realpath = d['realpath']
+        print('writing to %r' % realpath)
+        with open(realpath, 'w') as f:
+            f.write(data)
+        # reload
+        self._update_file(realpath)
+        
--- src/mcdp_web/main.py
+++ src/mcdp_web/main.py
@@ -1,11 +1,13 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response, FileResponse
-from conf_tools.global_config import GlobalConfig
 from quickapp.quick_app_base import QuickAppBase
 from mcdp_library.library import MCDPLibrary
-from pyramid.view import view_config
 from mocdp.dp_report.html import ast_to_html
+from mocdp.exceptions import DPSemanticError, DPSyntaxError
+from pyramid.request import Request
+from pyramid.renderers import render_to_response
+from mocdp.dp_report.gg_ndp import STYLE_GREENREDSYM
 
 __all__ = ['mcdp_web_main']
 
@@ -16,6 +18,7 @@ class WebApp():
         self.counter = 0
     
         self.library = None
+
 
     def get_library(self):
         if self.library is None:
@@ -35,9 +38,10 @@ class WebApp():
         models = self.list_of_models()
         return {'models': sorted(models)}
 
-    def view_model_ndp_graph(self, request):
+    def view_model_ndp_graph_image(self, request):
         model_name = str(request.matchdict['model_name'])  # unicod
         style = request.matchdict['style']
+        fileformat = request.matchdict['format']
 
         from mocdp.dp_report.gg_ndp import gvgen_from_ndp
         from cdpview.plot import png_pdf_from_gg
@@ -46,18 +50,29 @@ class WebApp():
         gg = gvgen_from_ndp(ndp, style)
         png, pdf = png_pdf_from_gg(gg)
 
-        import tempfile
-        with tempfile.NamedTemporaryFile(suffix='.png') as tf:
-            fn = tf.name
-            with open(fn, 'wb') as f:
-                f.write(png)
+        if fileformat == 'pdf':
+            return response_data(request=request, data=pdf, content_type='image/pdf')
+        elif fileformat == 'png':
+            return response_data(request=request, data=png, content_type='image/png')
+        else:
+            raise ValueError('No known format %r.' % fileformat)
 
-            response = FileResponse(fn, request=request, content_type='image/png')
 
-        return response
+    def view_model_ndp_graph(self, request):
+        model_name = str(request.matchdict['model_name'])  # unicode
+        models = self.list_of_models()
+        
+        return {'model_name': model_name,
+                'models': models,
+                'views': self._get_views(),
+                'current_view': 'ndp_graph',
+                'style': STYLE_GREENREDSYM,
+                }
 
     def view_model_syntax(self, request):
+        models = self.list_of_models()
         model_name = str(request.matchdict['model_name'])  # unicode
+
         filename = '%s.mcdp' % model_name
         l = self.get_library()
         f = l._get_file_data(filename)
@@ -69,10 +84,63 @@ class WebApp():
         return {'source_code': source_code,
                 'highlight': highlight,
                 'model_name': model_name,
-                'realpath': realpath}
+                'realpath': realpath,
+                'models': models,
+                'views': self._get_views(),
+                'current_view': 'syntax'}
+
+    def _get_views(self):
+        return ['syntax', 'ndp_graph']
+
+    def view_edit_form(self, request):
+        model_name = str(request.matchdict['model_name'])  # unicode
+
+        filename = '%s.mcdp' % model_name
+        l = self.get_library()
+        f = l._get_file_data(filename)
+        source_code = f['data']
+        realpath = f['realpath']
+        nrows = int(len(source_code.split('\n')) + 6)
+
+        return {'source_code': source_code,
+                'model_name': model_name,
+                'realpath': realpath,
+                'rows': nrows,
+                'error': None}
+
+    def view_edit_submit(self, request):
+        model_name = str(request.matchdict['model_name'])  # unicode
+
+        filename = '%s.mcdp' % model_name
+        l = self.get_library()
+        f = l._get_file_data(filename)
+        realpath = f['realpath']
+
+        data = str(request.params['source_code'])
+        data = data.replace('\r', '')
+        # validation:
+        try:
+            _parsed = l._actual_load(data, realpath=None)
+        except (DPSemanticError, DPSyntaxError) as e:
+            error = str(e)
+            nrows = int(len(data.split('\n')) + 6)
+            params = {'source_code': data,
+                      'model_name': model_name,
+                      'error': error,
+                      'rows': nrows}
+
+            return render_to_response('edit_form.jinja2',
+                                      params, request=request)
+
+        l.write_to_model(model_name, data)
+
+        subreq = Request.blank('/models/%s/syntax' % model_name)
+        response = request.invoke_subrequest(subreq)
+        return response
 
     def serve(self):
         config = Configurator()
+        config.add_static_view(name='static', path='static')
         config.include('pyramid_jinja2')
 
         config.add_route('index', '/')
@@ -84,12 +152,23 @@ class WebApp():
         config.add_route('empty', '/empty')
         config.add_view(self.view_index, route_name='empty', renderer='empty.jinja2')
 
+        config.add_route('edit_form', '/edit/{model_name}')
+        config.add_view(self.view_edit_form, route_name='edit_form',
+                        renderer='edit_form.jinja2')
+
+        config.add_route('edit_submit', '/edit_submit/{model_name}')
+        config.add_view(self.view_edit_submit, route_name='edit_submit')
+
         config.add_route('model_syntax', '/models/{model_name}/syntax')
         config.add_view(self.view_model_syntax, route_name='model_syntax',
                         renderer='model_syntax.jinja2')
 
-        config.add_route('model_ndp_graph', '/models/{model_name}/ndp_graph/{style}')
-        config.add_view(self.view_model_ndp_graph, route_name='model_ndp_graph')
+        config.add_route('model_ndp_graph', '/models/{model_name}/ndp_graph')
+        config.add_view(self.view_model_ndp_graph, route_name='model_ndp_graph',
+                        renderer='model_ndp_graph.jinja2')
+
+        config.add_route('model_ndp_graph_image', '/models/{model_name}/ndp_graph/image/{style}.{format}')
+        config.add_view(self.view_model_ndp_graph_image, route_name='model_ndp_graph_image')
 
         app = config.make_wsgi_app()
         server = make_server('0.0.0.0', 8080, app)
@@ -109,3 +188,14 @@ class MCDPWeb(QuickAppBase):
 
 mcdp_web_main = MCDPWeb.get_sys_main()
 
+
+
+def response_data(request, data, content_type):
+    import tempfile
+    with tempfile.NamedTemporaryFile() as tf:
+        fn = tf.name
+        with open(fn, 'wb') as f:
+            f.write(data)
+        response = FileResponse(fn, request=request, content_type=content_type)
+
+    return response
