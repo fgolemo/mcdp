161e738356418b547b0adf44d84cf0e5f422d100
Andrea Censi <censi@mit.edu>
2016-06-21 09:54:39 -0400

    adding dir to sys path

--- src/mocdp/__init__.py
+++ src/mocdp/__init__.py
@@ -8,3 +8,19 @@ logger.setLevel(logging.DEBUG)
 
 import numpy
 numpy.seterr('raise')
+
+
+def add_vendor():
+    import os
+    import sys
+
+    # Add vendor directory to module search path
+    parent_dir = os.path.abspath(os.path.dirname(__file__))
+    vendor_dir = os.path.join(parent_dir, 'vendor')
+
+    assert os.path.exists(vendor_dir)
+
+    sys.path.insert(0, vendor_dir)
+
+
+add_vendor()
