1e0211fc5bbdc43af215fce274e526001d5260d4
Andrea Censi <censi@mit.edu>
2016-06-25 19:32:55 -0400

    done

--- upload_tag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-python setup.py sdist upload
-version=`python -c "import mocdp; print mocdp.__version__"`
-git commit -a -m "tagging version ${version}"
-git tag "v${version}"
-git push --tags
