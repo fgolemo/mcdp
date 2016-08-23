7abbb2b4a2f4c58da653448acecfccc8c84611c5
Andrea Censi <censi@mit.edu>
2016-08-23 18:38:01 -0400

    done

--- src/mcdp_lang_tests/syntax_import.py
+++ src/mcdp_lang_tests/syntax_import.py
@@ -4,26 +4,6 @@ from mcdp_lang.syntax import Syntax
 from mcdp_lang_tests.utils import parse_wrap_check
 
 CDP = CDPLanguage
-#
-# def parse_import(s):
-#     """ Parses an import statement """
-#     expr = Syntax.import_statement
-#     return parse_wrap_check(string=s, expr=expr)
-#
-# @comptest
-# def syntax_import1():
-#     im = parse_import("from libname import a")
-#     assert isinstance(im, lang.ImportSymbols)
-#     with check_properties(im):
-#         symbols = [_.value for _ in unwrap_list(im.symbols)]
-#         assert im.libname.value == 'libname'
-#         assert symbols == ['a']
-#
-#     im = parse_import("from libname import a, b")
-#     with check_properties(im):
-#         symbols = [_.value for _ in unwrap_list(im.symbols)]
-#         assert im.libname.value == 'libname'
-#         assert symbols == ['a', 'b']
 
 @comptest
 def syntax_import1():
