2eacc0ce62021c4efd3ff11473e2e5507e683e91
Andrea Censi <censi@mit.edu>
2016-11-08 18:04:45 -0500

    fix

--- src/mcdp_maps/plus_value_map.py
+++ src/mcdp_maps/plus_value_map.py
@@ -343,7 +343,6 @@ class MinusValueNatMap(Map):
         if is_top(dom, self.c):
             return 0 
         
-        check_isinstance(x, int)
         if dom.leq(x, self.c):
             return 0
         else:
