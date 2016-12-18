8bdb83c661ee8b472c2da85bd7b40605b40d7ada
Andrea Censi <censi@mit.edu>
2016-12-17 19:42:57 -0500

    done

---
+++ src/mcdp_web/renderdoc/xmlutils.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+
+# def bs(fragment):
+#     return BeautifulSoup(fragment, 'html.parser', from_encoding='utf-8')
+def bs(fragment):
+    """ Returns the contents wrapped in an element called "fragment" """
+    s = '<fragment>%s</fragment>' % fragment
+    parsed = BeautifulSoup(s, 'lxml', from_encoding='utf-8')
+    res = parsed.html.body.fragment
+    assert res.name == 'fragment'
+    return res
+
+def to_html_stripping_fragment(soup):
+    assert soup.name == 'fragment'
+    s = str(soup)
+    s = s.replace('<fragment>','')
+    s = s.replace('</fragment>','')
+    return s
