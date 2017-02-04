19e98231ee3530430b7f8914be79602e624ca85d
Andrea Censi <censi@mit.edu>
2017-02-04 18:49:19 +0100

    done

--- src/mcdp/branch_info.py
+++ src/mcdp/branch_info.py
@@ -1,5 +1,5 @@
 
 class BranchInfo:
     """ Information about this particular branch """
-    branch_name = "inst00"
-    branch_topic = "Production"
+    branch_name = "manual"
+    branch_topic = "Manual"
