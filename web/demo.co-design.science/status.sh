c345ab23e9858fb4cf01e0cd05b66e09215cf1e0
Ubuntu user @ frankfurt.co-design <ubuntu@frankfurt.co-design.science>
2017-02-07 18:19:52 +0100

    done

---
+++ web/demo.co-design.science/status.sh
@@ -0,0 +1,2 @@
+echo `date`
+wget -O/home/ubuntu/inst00/src/mcdp/web/demo.co-design.science/status.png "https://api.browshot.com/api/v1/simple?key=OGJL7hwDXO47n2LOWRnBZHyaG2p&url=http%3A//status.co-design.science&size=page"
