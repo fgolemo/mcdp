f0f89e7702930ffb9bb1c094245ab858af4916ef
Ubuntu user @ frankfurt.co-design <ubuntu@frankfurt.co-design.science>
2017-02-21 08:24:34 +0100

    demo

--- web/demo.co-design.science/status.sh
+++ web/demo.co-design.science/status.sh
@@ -1,2 +1,11 @@
 echo `date`
-wget -O/home/ubuntu/inst00/src/mcdp/web/demo.co-design.science/status.png "https://api.browshot.com/api/v1/simple?key=OGJL7hwDXO47n2LOWRnBZHyaG2p&url=http%3A//status.co-design.science&size=page"
+dir=`dirname $0`
+
+out=${dir}/status.png
+tmp=${dir}/status.tmp.png
+
+wget -O${tmp} "https://api.browshot.com/api/v1/simple?key=OGJL7hwDXO47n2LOWRnBZHyaG2p&url=http%3A//status.co-design.science&size=page"
+
+mv ${tmp} ${out}
+
+echo `date` > ${dir}/status.timestamp.txt
