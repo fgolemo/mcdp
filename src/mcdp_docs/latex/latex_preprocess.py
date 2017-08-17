46dd71e779e6d8db8ff31c7da6317364038dcc80
Andrea Censi <acensi@ethz.ch>
2017-08-17 12:03:20 +0200

    minor

--- src/mcdp_docs/latex/latex_preprocess.py
+++ src/mcdp_docs/latex/latex_preprocess.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
+from contracts import contract
+from mcdp import logger
+from mcdp.exceptions import DPSyntaxError
+from mcdp_docs.latex.latex_inside_equation_abbrevs import replace_inside_equations
+from mcdp_docs.manual_constants import MCDPManualConstants
+from mcdp_docs.mark.markdown_transform import is_inside_markdown_quoted_block
+from mcdp_utils_misc.string_utils import get_md5
 import os
 import re
 
-from contracts import contract
 from contracts.interface import Where
 from contracts.utils import raise_desc, raise_wrapped, check_isinstance
-from mcdp import logger
-from mcdp.exceptions import DPSyntaxError
-from mcdp_utils_misc.string_utils import get_md5
-
-from mcdp_docs.latex.latex_inside_equation_abbrevs import replace_inside_equations
-from mcdp_docs.mark.markdown_transform import is_inside_markdown_quoted_block
-import warnings
-from mcdp_docs.manual_constants import MCDPManualConstants
 
 
 class LatexProcessingConstants():
