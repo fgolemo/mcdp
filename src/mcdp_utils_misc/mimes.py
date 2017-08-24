2e4fcf3cc37474fcba89ba02b947d441fb30b9a6
Andrea Censi <acensi@ethz.ch>
2017-08-24 23:45:38 +0200

    done

--- src/mcdp_utils_misc/mimes.py
+++ src/mcdp_utils_misc/mimes.py
@@ -1,6 +1,11 @@
 
+from mcdp import logger
+
 def get_mime_for_format(data_format):
     table = get_mime_table()
+    if data_format.lower() != data_format:
+        logger.error('You should not use uppercase (%s)'  % data_format)
+        data_format =data_format.lower()
     return table[data_format]
 
 def get_mime_table():
