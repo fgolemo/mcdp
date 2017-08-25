e2745d2032f33b45cfa2a5bcb3f93b29bf3f6670
Andrea Censi <acensi@ethz.ch>
2017-08-25 16:05:30 +0200

    (empty)

--- src/mcdp_docs/check_bad_input_files.py
+++ src/mcdp_docs/check_bad_input_files.py
@@ -2,6 +2,7 @@ from mcdp_utils_misc.locate_files_imp import locate_files
 from collections import defaultdict
 import os
 from compmake.utils.friendly_path_imp import friendly_path
+from mcdp_report.gg_utils import check_not_lfs_pointer
 
 def collect_by_extension(d):
     fs = locate_files(d, '*')
@@ -49,8 +50,10 @@ def check_lfs_checkout(ext2filenames):
      '.JPG', '.JPEG', '.jpg', '.PDF' ,'.pdf', '.PNG', '.png']
     for ext in lfs_extensions:
         for filenames in ext2filenames[ext]:
-            # check_not_lfs_pointer(fn)
-            pass
+            for fn in filenames:
+                data = open(fn).read()
+                check_not_lfs_pointer(fn, data)
+            
    
     
     
